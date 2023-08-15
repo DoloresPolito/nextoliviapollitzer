47d94ddd582254fd7ebd8cab96968770bb22863e
Dolores Polito <dolores.polito@gmail.com>
2023-08-15 15:03:34 -0300

    logo mobile

--- src/sections/Cover.js
+++ src/sections/Cover.js
@@ -25,54 +25,23 @@ const Cover = () => {
     <>
       {width >= small ? (
         <>
-          <CoverSection >
+          <CoverSection>
             <Image src={logo} alt="logo" className="logo" />
           </CoverSection>
         </>
       ) : (
         <>
-          <CoverSectionMobile ></CoverSectionMobile>
+          <CoverSectionMobile>
+          <Image src={logo} alt="logo" className="logo" />
+
+          </CoverSectionMobile>
         </>
       )}
     </>
   );
 };
 
-// const Page = styled.div`
-//   height: 100%;
-//   width: 100%;
-// `;
 
-// const Logo = styled.img`
-//   position: absolute;
-//   height: 600px;
-//   width: 600px;
-//   top: 40px;
-
-//   @media only screen and (max-width: 1160px) {
-//     height: 500px;
-//     width: 500px;
-//     top: 80px;
-//   }
-
-//   @media only screen and (max-width: 700px) {
-//     height: 400px;
-//     width: 400px;
-//     top: 110px;
-//   }
-
-//   @media only screen and (max-width: 450px) {
-//     height: 350px;
-//     width: 350px;
-//     top: 15vh !important;
-//   }
-
-//   @media only screen and (max-width: 350px) {
-//     height: 300px;
-//     width: 300px;
-//     top: 15vh !important;
-//   }
-// `;
 const CoverSection = styled.div`
   width: 100%;
   height: 100vh !important;
@@ -108,16 +77,12 @@ const CoverSection = styled.div`
       top: 15vh !important;
     }
 
-    @media only screen and (max-width: 350px) {
-      height: 300px;
-      width: 300px;
-      top: 15vh !important;
-    }
+   
   }
 
-  @media only screen and (max-width: 450px) {
+  /* @media only screen and (max-width: 450px) {
     height: 500px !important;
-  }
+  } */
 `;
 const CoverSectionMobile = styled.div`
   width: 100%;
@@ -129,7 +94,23 @@ const CoverSectionMobile = styled.div`
   background-image: url("/assets/pictures/heroextralowmobile.webp");
   background-size: cover;
   /* height: calc(100vh - 100px); */
-  height: 80vh;
+  height: 70vh;
+
+  .logo{
+
+    @media only screen and (max-width: 450px) {
+      height: 350px;
+      width: 350px;
+      /* top: 15vh !important; */
+      margin-top: 50px !important;
+    }
+    @media only screen and (max-width: 350px) {
+      height: 280px;
+      width: 280px;
+      /* top: 15vh !important; */
+      margin-top: 80px !important;
+    }
+  }
 `;
 
 export default Cover;
